3aaece652af9c41275da6b432b0e513ef2499e1c
unknown <hugofabien.martin@gmail.com>
2023-11-20 17:22:29 +0100

    adding hilt for di

---
+++ app/src/main/java/com/hugofab/departementle/di/DepartementleApplication.kt
@@ -0,0 +1,8 @@
+package com.hugofab.departementle.di
+
+import android.app.Application
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class DepartementleApplication: Application() {
+}
